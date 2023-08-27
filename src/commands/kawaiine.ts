429c7c24df3500102a1208843954b94ff23cf766
Omuretsu <75904606+Omuretsu@users.noreply.github.com>
2023-08-27 20:22:00 +0900

    feat: kawaiineコマンドを実装 (#53)
    * feat: kawaiineコマンドを実装
    
    * feat: kowaineコマンドを実装
    
    * chore: 他のPRで実装する予定の、kowaineコマンドを削除
    
    * fix: コードの修正
    
    ---------
    
    Co-authored-by: Tomachi <8929706+book000@users.noreply.github.com>

---
+++ src/commands/kawaiine.ts
@@ -0,0 +1,17 @@
+import { Discord } from '@/discord'
+import { Message } from 'discord.js'
+import { BaseCommand, Permission } from '.'
+
+export class KawaiineCommand implements BaseCommand {
+  get name(): string {
+    return 'kawaiine'
+  }
+
+  get permissions(): Permission[] | null {
+    return null
+  }
+
+  async execute(_discord: Discord, message: Message<boolean>): Promise<void> {
+    message.reply('ｶﾜｲｲﾈ!')
+  }
+}
