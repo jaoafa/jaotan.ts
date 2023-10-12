f4aea5c9ddc2310b9a5565a5d8093a3a3f7166ae
Omelet <75904606+Omuretsu@users.noreply.github.com>
2023-10-13 00:10:33 +0900

    fix: GIFファイルへのリンクを削除

--- src/commands/akakese.ts
+++ src/commands/akakese.ts
@@ -19,7 +19,7 @@ export class AkakeseCommand implements BaseCommand {
     _args: string[]
   ): Promise<void> {
     await message.reply(
-      'なンだおまえ!!!!帰れこのやろう!!!!!!!!人間の分際で!!!!!!!!寄るな触るな近づくな!!!!!!!!垢消せ!!!!垢消せ!!!!!!!! ┗(‘o’≡’o’)┛!!!!!!!!!!!!!!!! https://twitter.com/settings/accounts/confirm_deactivation https://media.discordapp.net/attachments/623153228267388958/1145366955218448404/akakese.gif'
+      'なンだおまえ!!!!帰れこのやろう!!!!!!!!人間の分際で!!!!!!!!寄るな触るな近づくな!!!!!!!!垢消せ!!!!垢消せ!!!!!!!! ┗(‘o’≡’o’)┛!!!!!!!!!!!!!!!! https://twitter.com/settings/accounts/confirm_deactivation'
     )
   }
 }
