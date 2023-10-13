5e34b56a7fe64370b091a459e1345cec74b00d86
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2023-10-13 14:20:47 +0000

    feat: とりあえず承認・否認処理前まで実装

--- src/features/meeting-vote.ts
+++ src/features/meeting-vote.ts
@@ -1,10 +1,146 @@
-import { Collection, Message, User } from 'discord.js'
+import { Collection, Message, TextChannel, User } from 'discord.js'
+
+/**
+ * 否認理由
+ */
+const DisapprovalReason = {
+  /**
+   * 投票による否認
+   */
+  Vote: '投票による否認',
+  /**
+   * 期限による否認
+   */
+  Limit: '期限による否認',
+} as const
 
 /**
  * meeting_voteチャンネルの機能
  */
 export class MeetingVote {
-  // TODO: つくる
+  /** 投票権利を有するメンバー */
+  private readonly memberIds = [
+        "206692134991036416", // zakuro
+        "221498004505362433", // hiratake
+        "221991565567066112", // tomachi
+        "222337959087702016", // omelet
+        "492088741167366144" // yuua
+  ]
+
+  /** メッセージテキストの境界値正規表現 */
+  private readonly borderRegex = /\[Border:(\d+)]/
+
+  public async run(channel: TextChannel) {
+    const pinnedMessages = await channel.messages.fetchPinned()
+    for (const message of pinnedMessages.values()) {
+      await this.runMessage(message)
+    }
+  }
+
+  public async runMessage(message: Message) {
+    const goodUsers = await VoteReactionType.Good.getUsers(message)
+    const badUsers = await VoteReactionType.Bad.getUsers(message)
+    const whiteUsers = await VoteReactionType.White.getUsers(message)
+
+    const border = this.calculateBorder(message, whiteUsers)
+    if(goodUsers.size >= border) {
+      await this.approval(message, goodUsers, badUsers, whiteUsers)
+      return
+    } else if (badUsers.size >= border) {
+      await this.disapproval(message, goodUsers, badUsers, whiteUsers, DisapprovalReason.Vote)
+      return
+    }
+
+    // 投票開始から2週間が経過していて、それまでに確定しなかった場合、投票を締め切り否認とする
+    const twoWeeksAgo = new Date()
+    twoWeeksAgo.setDate(twoWeeksAgo.getDate() - 14)
+    
+    if (message.createdAt < twoWeeksAgo) {
+      await this.disapproval(message, goodUsers, badUsers, whiteUsers, DisapprovalReason.Limit)
+      return
+    }
+
+    // 投票開始から1週間が経過していて、リマインド済みでない場合、リマインドする
+    // リマインドした場合、Bot自身がリアクションをつける
+    const reminded = await VoteReactionType.Remind.isReacted(message, message.client.user.id)
+    if (!reminded) {
+      await this.remind(message, goodUsers, badUsers, whiteUsers)
+    }
+  }
+
+  /**
+   * 投票メッセージを承認します。
+   * 
+   * @param message メッセージ
+   * @param goodUsers 賛成票を投票したユーザーのコレクション
+   * @param badUsers 反対票を投票したユーザーのコレクション
+   * @param whiteUsers 白票を投票したユーザーのコレクション
+   */
+  private async approval(
+    message: Message,
+    goodUsers: Collection<string, User>,
+    badUsers: Collection<string, User>,
+    whiteUsers: Collection<string, User>
+  ) {
+    // TODO
+  }
+
+  /**
+   * 投票メッセージを否認します。
+   * 
+   * @param message メッセージ
+   * @param goodUsers 賛成票を投票したユーザーのコレクション
+   * @param badUsers 反対票を投票したユーザーのコレクション
+   * @param whiteUsers 白票を投票したユーザーのコレクション
+   * @param reason 否認理由
+   */
+  private async disapproval(
+    message: Message,
+    goodUsers: Collection<string, User>,
+    badUsers: Collection<string, User>,
+    whiteUsers: Collection<string, User>,
+    reason: (typeof DisapprovalReason)[keyof typeof DisapprovalReason]
+  ) {
+    // TODO
+  }
+
+  /**
+   * 投票メッセージをリマインドします。
+   * 
+   * @param message メッセージ
+   * @param goodUsers 賛成票を投票したユーザーのコレクション
+   * @param badUsers 反対票を投票したユーザーのコレクション
+   * @param whiteUsers 白票を投票したユーザーのコレクション
+   */
+  private async remind(
+    message: Message,
+    goodUsers: Collection<string, User>,
+    badUsers: Collection<string, User>,
+    whiteUsers: Collection<string, User>
+  ) {
+    // TODO
+  }
+
+  /**
+   * 承認・否認が確定する境界値を計算します。
+   * 
+   * 投票権利を有するユーザー数の半数以上が承認した場合、確定します。
+   * 但し、白票は投票権利を有するユーザー数に含まれません。
+   * 
+   * @param message メッセージ
+   * @param whiteUsers 白票を投票したユーザーのコレクション
+   */
+  private calculateBorder(message: Message, whiteUsers: Collection<string, User>) {
+    const match = message.content.match(this.borderRegex)
+    if (match && match.length > 1) {
+      return Number.parseInt(match[1])
+    }
+
+    const whiteCount = whiteUsers.size
+    const totalCount = this.memberIds.length
+
+    return ((totalCount - whiteCount) / 2) + 1
+  }
 }
 
 /**
@@ -17,13 +153,25 @@ class VoteReaction {
     this.unicode = unicode
   }
 
+  /**
+   * メッセージにリアクションを付与します。
+   * 
+   * @param message メッセージ
+   */
   public async addReaction(message: Message): Promise<void> {
     await message.react(this.unicode)
   }
 
+  /**
+   * 指定したメッセージでこのリアクションをつけたユーザーを取得します。
+   * 
+   * @param message メッセージ
+   * @param excludeBot Botを除外するかどうか
+   * @returns ユーザーのコレクション
+   */
   public async getUsers(
     message: Message,
-    botOnly = true
+    excludeBot = false
   ): Promise<Collection<string, User>> {
     const reactions = message.reactions.cache.find(
       (reaction) => reaction.emoji.name === this.unicode
@@ -32,9 +180,16 @@ class VoteReaction {
       return new Collection<string, User>()
     }
     const users = await reactions.users.fetch()
-    return botOnly ? users.filter((user: User) => user.bot) : users
+    return excludeBot ? users.filter((user: User) => !user.bot) : users
   }
 
+  /**
+   * ユーザーが指定したメッセージにこのリアクションをつけているかどうかを取得します。
+   * 
+   * @param message メッセージ
+   * @param userId ユーザーID
+   * @returns リアクションをつけているかどうか
+   */
   public async isReacted(message: Message, userId: string): Promise<boolean> {
     const users = await this.getUsers(message)
     return users.some((user: User) => user.id === userId)
@@ -45,8 +200,20 @@ class VoteReaction {
  * 投票に利用するリアクション種別
  */
 export const VoteReactionType = {
+  /**
+   * 賛成
+   */
   Good: new VoteReaction('\uD83D\uDC4D'),
+  /**
+   * 反対
+   */
   Bad: new VoteReaction('\uD83D\uDC4E'),
+  /**
+   * 白票
+   */
   White: new VoteReaction('\uD83C\uDFF3'),
+  /**
+   * リマインド済み
+   */
   Remind: new VoteReaction('\uD83D\uDCF3'),
 } as const
