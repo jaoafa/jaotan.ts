2281c8cf3c514c33d0f00a4dc2624ca9e3bf3949
yuua <identity@yuua.dev>
2023-10-28 23:22:46 +0900

    fix: isMultipleVote usage

--- src/events/meeting-vote-reaction.ts
+++ src/events/meeting-vote-reaction.ts
@@ -58,7 +58,7 @@ export class MeetingReactionVoteEvent extends BaseDiscordEvent<'messageReactionA
     }
 
     // 複数のリアクションをつけている場合は、リアクションを削除してメッセージを返す
-    if (!(await meetingVoteFeature.isMultipleVote(message, user))) {
+    if (await meetingVoteFeature.isMultipleVote(message, user)) {
       await this.executeMultipleVote(message, reaction, user)
       return
     }
