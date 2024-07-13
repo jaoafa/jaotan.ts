8cc7841dc978865269e289d8816f4fd77c80a518
Tomachi <8929706+book000@users.noreply.github.com>
2024-07-13 23:58:23 +0900

    feat: unmute command (#829)

---
+++ src/commands/unmute.ts
@@ -0,0 +1,36 @@
+import { Discord } from '@/discord'
+import { Message } from 'discord.js'
+import { BaseCommand } from '.'
+
+export class UnmuteCommand implements BaseCommand {
+  readonly name = 'unmute'
+  readonly permissions = null
+
+  async execute(_discord: Discord, message: Message): Promise<void> {
+    // サーバミュートを解除する
+    const member = message.member
+    if (!member) {
+      await message.reply({
+        embeds: [
+          {
+            title: 'エラー',
+            description: 'メンバー情報を取得できませんでした。',
+            color: 0xff_00_00,
+          },
+        ],
+      })
+      return
+    }
+
+    await member.voice.setMute(false)
+    await message.reply({
+      embeds: [
+        {
+          title: 'サーバミュート解除成功',
+          description: 'あなたのサーバミュートを解除しました。',
+          color: 0x00_ff_00,
+        },
+      ],
+    })
+  }
+}
